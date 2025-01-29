8145ac1e723641c0e42130bdc30ad2a19dbf4c8d
renbaum <renbaum04@bluewin.ch>
2025-01-29 08:13:34 +0100

    with bi-grade

--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -1,5 +1,6 @@
 # Write your code here
 import re
+from nltk import ngrams
 import sys
 from os.path import expanduser
 
@@ -8,11 +9,15 @@ filename = input()
 with open(filename, "r", encoding="utf-8") as file:
     corpus = file.read()
 
-tokens = re.split(r"[\s ]", corpus)
+n = 2
+n_grams = ngrams(corpus.split(), n)
+#tokens = re.split(r"[\s ]", corpus)
+lst = [grams for grams in n_grams]
+print(f"Number of bigrams: {len(lst)}")
 
-print("Corpus statistics")
-print(f"All tokens: {len(tokens)}")
-print(f"Unique tokens: {len(set(tokens))}")
+#print("Corpus statistics")
+#print(f"All tokens: {len(tokens)}")
+#print(f"Unique tokens: {len(set(tokens))}")
 
 print()
 while True:
@@ -23,7 +28,7 @@ while True:
         case _:
             try:
                 index = int(cmd)
-                print(tokens[index])
+                print(f"Head: {lst[index][0]}\tTail: {lst[index][1]}")
             except TypeError:
                 print("Type Error. Please input an integer.")
             except ValueError:
