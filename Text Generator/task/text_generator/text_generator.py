2d748054622206c38d8e7880a15909cbd3866e43
renbaum <renbaum04@bluewin.ch>
2025-01-29 08:32:33 +0100

    Markoc chain

--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -4,6 +4,47 @@ from nltk import ngrams
 import sys
 from os.path import expanduser
 
+class Tails:
+    def __init__(self, tail: str = None):
+        self.tails = {}
+        if tail is not None:
+            self.add(tail)
+
+    def add(self, tail):
+        if tail in self.tails:
+            self.tails[tail] += 1
+        else:
+            self.tails[tail] = 1
+
+    def print_tails(self):
+        for tail, count in self.tails.items():
+            print(f"Tail: {tail}\tCount: {count}")
+
+
+
+class Bigrams:
+    def __init__(self, bigram: tuple = None):
+        self.bigrams = {}
+        if bigram is not None:
+            self.add(bigram)
+
+    def add(self, bigram):
+        head, tail = bigram
+        if head not in self.bigrams:
+            self.bigrams[head] = Tails(tail)
+        else:
+            self.bigrams[head].add(tail)
+
+    def print_bigrams(self, head: str):
+        print(f"Head: {head}")
+        if head not in self.bigrams:
+            print("Key Error. The requested word is not in the model. Please input another word.")
+            print()
+            return
+
+        self.bigrams[head].print_tails()
+        print()
+
 filename = input()
 
 with open(filename, "r", encoding="utf-8") as file:
@@ -12,14 +53,17 @@ with open(filename, "r", encoding="utf-8") as file:
 n = 2
 n_grams = ngrams(corpus.split(), n)
 #tokens = re.split(r"[\s ]", corpus)
-lst = [grams for grams in n_grams]
-print(f"Number of bigrams: {len(lst)}")
+b = Bigrams()
+for grams in n_grams:
+    b.add(grams)
+#lst = [Bigrams(grams) for grams in n_grams]
+
+#print(f"Number of bigrams: {len(lst)}")
 
 #print("Corpus statistics")
 #print(f"All tokens: {len(tokens)}")
 #print(f"Unique tokens: {len(set(tokens))}")
 
-print()
 while True:
     cmd = input()
     match cmd.lower():
@@ -27,8 +71,7 @@ while True:
             break
         case _:
             try:
-                index = int(cmd)
-                print(f"Head: {lst[index][0]}\tTail: {lst[index][1]}")
+                b.print_bigrams(cmd)
             except TypeError:
                 print("Type Error. Please input an integer.")
             except ValueError:
